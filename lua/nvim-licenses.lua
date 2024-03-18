b701d8eecad37fcd193e4074f05a97aaab425a0f
kilavila <christer.kilavik@protonmail.com>
2024-03-18 11:01:33 +0100

    license body testing

--- lua/nvim-licenses.lua
+++ lua/nvim-licenses.lua
@@ -127,7 +127,7 @@ local function select_license()
 
   -- local source = license_json:match('"body": "(.-)"')
   -- match "body": (.-) to end of line
-  local source = license_json:match('"body":(.-)$')
+  local source = license_json:match('"body":(.-)",$')
   local license = string.gsub(source, '\\n', '\n')
 
   local file = io.open('LICENSE.md', 'w')
